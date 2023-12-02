95d688f90bba4bba45d058851e6d3665e08da040
Bryan Snyder <snyderb.de@gmail.com>
2023-12-02 12:28:07 -0500

    stage 2 working

--- file-dupe-handler.go
+++ file-dupe-handler.go
@@ -1,6 +1,9 @@
 package main
 
+// Stage 2 working correctly
+
 import (
+	"bufio"
 	"fmt"
 	_ "fmt"
 	_ "io/fs"
@@ -8,9 +11,13 @@ import (
 	_ "os"
 	"path/filepath"
 	_ "path/filepath"
+	"sort"
 )
 
-func listFilesAndFolders(directory string) {
+func listFilesAndFolders(directory string, fileFormat string, descending bool) {
+	// Create a map to store files by size
+	filesBySize := make(map[int64][]string)
+
 	// Start by walking the specified directory
 	err := filepath.Walk(directory, func(path string, info os.FileInfo, err error) error {
 		if err != nil {
@@ -18,14 +25,34 @@ func listFilesAndFolders(directory string) {
 			return err
 		}
 		// Check if it's a directory or a file
-		if !info.IsDir() {
-			fmt.Println(path) // Print file path
+		if !info.IsDir() && (fileFormat == "" || filepath.Ext(path) == "."+fileFormat) {
+			size := info.Size() // Get file size
+			filesBySize[size] = append(filesBySize[size], path)
 		}
 		return nil
 	})
 
 	if err != nil {
 		fmt.Println("Error:", err)
+	}
+
+	// Sort the sizes in ascending order or descending order based on user input
+	sizes := make([]int64, 0, len(filesBySize))
+	for size := range filesBySize {
+		sizes = append(sizes, size)
+	}
+	if descending {
+		sort.Slice(sizes, func(i, j int) bool { return sizes[i] > sizes[j] })
+	} else {
+		sort.Slice(sizes, func(i, j int) bool { return sizes[i] < sizes[j] })
+	}
+
+	// Print files grouped by size
+	for _, size := range sizes {
+		fmt.Println(size, "bytes")
+		for _, path := range filesBySize[size] {
+			fmt.Println(path)
+		}
 	}
 }
 
@@ -39,6 +66,31 @@ func main() {
 	// Get the root directory from the command-line argument
 	directory := os.Args[1]
 
+	// Read user input for file format
+	fmt.Print("Enter file format: ")
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	fileFormat := scanner.Text()
+
+	// Read user input for sorting option
+	var sortingOption string
+	for {
+		fmt.Println("Size sorting options:")
+		fmt.Println("1. Descending")
+		fmt.Println("2. Ascending")
+		fmt.Print("Enter a sorting option: ")
+		scanner.Scan()
+		sortingOption = scanner.Text()
+		if sortingOption == "1" || sortingOption == "2" {
+			break
+		} else {
+			fmt.Println("Wrong option")
+		}
+	}
+
+	// Determine if sorting should be in descending order
+	descending := sortingOption == "1"
+
 	// Call the listFilesAndFolders function to list files in the specified directory
-	listFilesAndFolders(directory)
+	listFilesAndFolders(directory, fileFormat, descending)
 }
